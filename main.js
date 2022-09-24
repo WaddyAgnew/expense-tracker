4ceb450300ad7a7e7fde0584118e9d829f8093a5
WaddyAgnew <waddyagnew@gmail.com>
2022-09-24 07:57:48 -0400

    fix

--- main.js
+++ main.js
@@ -1,35 +1,62 @@
 
- var openlist= document.getElementsByTagName("LI");
-
-
-
-function list_Update()
-{
-var list = document.createElement('LI');
-var input = document.getElementById("text").value;
-var t = document.createTextNode(input);
-list.appendChild(t);
-if(input ==='') {
-false;
-} else {
-    document.getElementById("list").appendChild(list);
+/*
+function clearhistory(){
+  localStorage.clear();
 }
+ function list_Update(){
 
-document.getElementById("text").value ="";
+  
 
+    const item = document.getElementById('item').value,
+        amount = document.getElementById('amount').value,
+      listNode_1 = document.getElementById('list_1'),
+      listNode_2 = document.getElementById('list_2'),
+      liNode1 = document.createElement("LI"),
+      liNode_2 = document.createElement("LI"),
+      itemNode = document.createTextNode(item),
+      amountNode = document.createTextNode(amount);
 
-var span = document.createElement("SPAN");
-var txt = document.createTextNode("\u00D7");
-span.className = "close";
-span.appendChild(txt);
-list.appendChild(span); 
+      amount.forEach(amount => balance.add(amount));
 
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
+    
+
+      liNode_2.appendChild(itemNode);
+      liNode1.appendChild(amountNode);
+      listNode_1.appendChild(liNode1);
+      listNode_2.appendChild(liNode_2);
   }
+      */
+ 
+  const localStorageTransactions = JSON.parse(
+    localStorage.getItem("transactions")
+  );
+  let transactions = localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
+
+  let totalBalance = '$0.00~';
+
+function update(){
+
+const amount = document.getElementById('amount').value;
+const item = document.getElementById('item').value;
+const totalDisplay = document.getElementById('balanceDisplay');
+totalDisplay.textContent = totalBalance;
+
+const listNode = document.getElementById('list_1'),
+liNode = document.createElement("LI"),
+itemNode = document.createTextNode(item),
+amountNode = document.createTextNode(amount);
+
+liNode.appendChild(amountNode);
+liNode.appendChild(itemNode);
+listNode.appendChild(liNode);
+
+
 
 }
-document.getElementById('btn').onclick = list_Update;
+
+function updateBalance(){
+const amounts = transactions.map((transaction) => transaction.amount);
+
+const total = amounts.reduce((bal, value) =>(bal += value),0).toFixed(2);
+  
+}
